e1f918f0b856a45256e76d0cf865262cbbd4b24f
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2024-12-29 15:22:47 +0530

    updated commands

---
+++ Shell-Scripting/commands/which-and-locate.sh
@@ -0,0 +1,16 @@
+:<<'COMMENTS'
+
+"find" is a powerful and thorough utility that helps you to find any file no matter where it is stored
+
+"locate" is much faster, but works on a database that needs to be  defined using updatedb, and updated periodically
+
+"which" is useful to find the exact location of binary files from the $PATH variable
+
+COMMENTS
+
+
+which python
+which python3
+which passwd
+
+
