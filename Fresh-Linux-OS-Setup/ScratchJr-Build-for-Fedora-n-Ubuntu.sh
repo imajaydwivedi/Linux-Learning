33c4a20baed08fbf16f1b0bc9507fdeb762dbfe4
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2025-02-16 08:33:45 +0530

    Added commands to build scratchjr

---
+++ Fresh-Linux-OS-Setup/ScratchJr-Build-for-Fedora-n-Ubuntu.sh
@@ -0,0 +1,17 @@
+# Go to Github directory, and clone the ScratchJr-Desktop repository
+    # https://github.com/jfo8000/ScratchJr-Desktop/issues/44#issuecomment-852197567
+
+cd Github
+git clone https://github.com/leonskb4/ScratchJr-Desktop
+
+# Install Node.js and npm
+sudo apt-get install -y nodejs npm
+sudo dnf install -y nodejs npm
+
+# Install ScratchJr-Desktop dependencies
+cd ScratchJr-Desktop
+npm install && npm run publish
+
+# Output
+This should generate *.deb/*.rpm file in out/make.
+
